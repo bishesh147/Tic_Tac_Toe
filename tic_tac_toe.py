bde39618dd65cc7ab45bb0dc41ca1c6b6b86447a
Bishesh Adhikari <adhikaribishesh3030@gmail.com>
2024-06-20 16:54:39 -0400

    Made the tic-tac-toe lines

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -27,22 +27,31 @@ start_screen_run = True
 
 # Start screen
 while start_screen_run:
+    # Create a rectangle with (top-leftx, top-lefty, width, height)
     start_rect = pygame.Rect(200, 300, 400, 100)
 
+    # Goes over the events
     for event in pygame.event.get():
+        # Press the quit button at the top
         if event.type == pygame.QUIT:
             start_screen_run = False
             running = False
         
+        # Check if the mouse is clicking the start_rect rectangle
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if start_rect.collidepoint(event.pos):
                 start_screen_run = False
 
+    # Fill the screen with white color
     screen.fill(WHITE)
 
+    # Draw the rectangle
     pygame.draw.rect(screen, BLUE, start_rect)
+    
+    # Write the text
     screen.blit(start_text, (200, 300))
     
+    # Update the display
     pygame.display.flip()
 
 
@@ -52,6 +61,11 @@ while running:
             running = False
 
     screen.fill(WHITE)
+
+    # Drawing lines
+    for i in range(4):
+        pygame.draw.line(screen, BLACK, (150, 50+150*i), (600, 50+150*i), 10)
+        pygame.draw.line(screen, BLACK, (150+150*i, 50), (150+150*i, 500), 10)
 
     pygame.display.flip()
 pygame.quit()
